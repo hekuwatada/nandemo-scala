ef8366878c4a0bfeb344f33ed63b0657af8074e5
Heku Watada <heku.watada@gmail.com>
2019-02-03 01:07:49 +0000

    Added a tool to read from a topic from beginning.

--- build.sbt
+++ build.sbt
@@ -7,12 +7,17 @@ version := "0.1"
 scalaVersion := "2.12.8"
 
 val prodLibs = Seq(
-  "org.apache.kafka" % "kafka-clients" % "0.10.2.2"
+  "org.apache.kafka" % "kafka-clients" % "2.1.0",
+  "com.typesafe.akka" %% "akka-stream" % "2.5.20",
+  "com.typesafe.akka" %% "akka-actor" % "2.5.20",
+  "com.typesafe.akka" %% "akka-stream-kafka" % "1.0-RC1"
 )
 
 val testLibs = Seq(
   "org.scalactic" %% "scalactic" % "3.0.5",
-  "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+  "org.scalatest" %% "scalatest" % "3.0.5" % Test,
+  "com.typesafe.akka" %% "akka-stream-testkit" % "2.5.20" % Test,
+  "com.typesafe.akka" %% "akka-testkit" % "2.5.20" % Test
 )
 
 libraryDependencies ++= prodLibs ++ testLibs
---
+++ src/test/scala/org/nandemo/scala/kafka/stream/KafkaConsumerStreamTestTool.scala
@@ -0,0 +1,37 @@
+package org.nandemo.scala.kafka.stream
+
+import akka.Done
+import akka.actor.ActorSystem
+import akka.kafka.scaladsl.Consumer
+import akka.kafka.{ConsumerSettings, Subscriptions}
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import com.typesafe.config.ConfigFactory
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.nandemo.scala.kafka.client.LocalKafkaConf
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+object KafkaConsumerStreamTestTool extends App {
+
+  implicit val system = ActorSystem("kafka-consumer-stream")
+  implicit val materializer = ActorMaterializer()
+
+  val config = ConfigFactory.load()
+  val groupId = "group1"
+
+  val consumerSettings =
+    ConsumerSettings(config.getConfig("akka.kafka.consumer"), new StringDeserializer, new StringDeserializer)
+    .withBootstrapServers(LocalKafkaConf.LocalKafkaServer)
+    .withGroupId(groupId)
+
+  val source: Source[ConsumerRecord[String, String], Consumer.Control] = Consumer
+    .plainSource[String, String](consumerSettings, Subscriptions.topics(LocalKafkaConf.TestTopic))
+
+  val doneF: Future[Done] = source
+      .runForeach((record: ConsumerRecord[String, String]) => println(record.value()))
+
+  Await.result(doneF, 30 seconds)
+}
