c1eeecd5736587650064ed1f509cdfe3fb223ee1
Heku Watada <heku.watada@gmail.com>
2019-02-19 09:46:45 +0000

    Added return type signature to Kafka String sink.

--- src/main/scala/org/nandemo/scala/kafka/stream/KafkaStringStream.scala
+++ src/main/scala/org/nandemo/scala/kafka/stream/KafkaStringStream.scala
@@ -1,9 +1,13 @@
 package org.nandemo.scala.kafka.stream
 
+import akka.Done
 import akka.kafka.scaladsl.Consumer
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Sink, Source}
 import com.typesafe.config.Config
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.producer.ProducerRecord
+
+import scala.concurrent.Future
 
 object KafkaStringStream {
   implicit val serializationResolver = DefaultKafkaStringSerializationResolver
@@ -12,6 +16,6 @@ object KafkaStringStream {
                  (implicit config: Config): Source[ConsumerRecord[String, String], Consumer.Control] =
     KafkaStream.kafkaSource[String, String](topic, consumerGroupId)
 
-  def kafkaSink()(implicit config: Config) =
+  def kafkaSink()(implicit config: Config): Sink[ProducerRecord[String, String], Future[Done]] =
     KafkaStream.kafkaSink()
 }
